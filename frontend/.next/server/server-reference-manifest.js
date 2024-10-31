2fb486f87b4addd769cebf5a3a6be927fdb7b14e
dduamelo <david.dosseh@epitech.eu>
2024-10-31 21:14:57 +0100

    fix: fix npm packages

--- frontend/.next/server/server-reference-manifest.js
+++ frontend/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"IbBicLvMShDoqkaibwYj7f9WkReXMXS/zEs6FQHfU1s=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"\n}"

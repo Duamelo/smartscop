f9942710d7e212b51e16f139ff39c96448f4b4f4
samiramvo <mvosamira@gmail.com>
2024-11-02 00:12:32 +0100

    feat: implementation of envirronment

--- frontend/app/components/StartCard.tsx
+++ frontend/app/components/StartCard.tsx
@@ -9,10 +9,19 @@ interface StatCardProps {
 
 const StatCard: React.FC<StatCardProps> = ({ title, value, todayChange }) => {
   return (
-    <div className="bg-white p-4 rounded-md shadow-md ">
-      <h3 className="text-gray-600">{title}</h3>
-      <p className="text-3xl font-bold">{value}</p>
-      <p className="text-green-600">+{todayChange} aujourd'hui</p>
+    <div className="bg-white p-4 rounded-md shadow-md flex justify-between ">
+      <div>
+        <h3 className="text-gray-600 mb-4">{title}</h3>
+        <div className="flex gap-2">
+          <p className="bg-colorVert text-green-800 rounded-md p-1">
+            +{todayChange}
+          </p>
+          <p className="text-[14px] text-center">aujourd'hui</p>
+        </div>
+      </div>
+      <div>
+        <p className="text-3xl font-bold">{value}</p>
+      </div>
     </div>
   );
 };

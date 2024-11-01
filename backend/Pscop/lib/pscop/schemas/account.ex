e5fe17d90cd3adce1d20f337a4253364bc71ac36
dduamelo <david.dosseh@epitech.eu>
2024-11-01 18:58:15 +0100

    feat: add password validation

--- backend/Pscop/lib/pscop/schemas/account.ex
+++ backend/Pscop/lib/pscop/schemas/account.ex
@@ -3,6 +3,7 @@ defmodule Pscop.Schemas.Account do
   import Ecto.Changeset
 
   @email_regex ~r/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+  @password_regex ~r/(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_])/
 
   schema "accounts" do
     field :password_hash, :string
@@ -23,7 +24,23 @@ defmodule Pscop.Schemas.Account do
     |> validate_required([:email, :password, :role_id])
     |> validate_format(:email, @email_regex, message: "must be a valid email address")
     |> unique_constraint(:email)
+    |> validate_length(:password, min: 8, message: "password must be at least 8 characters long")
+    |> validate_password_format(:password)
     |> hash_password()
+  end
+
+  defp validate_password_format(changeset, field) do
+    password = get_field(changeset, field)
+
+    if password && !valid_password?(password) do
+      add_error(changeset, field, "password must contain at least one uppercase letter, one lowercase letter, one digit, and one special character")
+    else
+      changeset
+    end
+  end
+
+  defp valid_password?(password) do
+    Regex.match?(password_regex, password)
   end
 
   defp hash_password(changeset) do

d2cc1bb63a16dee4154d7f162e86a3ca7d3e0c7b
dduamelo <david.dosseh@epitech.eu>
2024-11-01 19:07:35 +0100

    feat: add authenticate_account method

--- backend/Pscop/lib/pscop/services/accounts.ex
+++ backend/Pscop/lib/pscop/services/accounts.ex
@@ -24,4 +24,18 @@ defmodule Pscop.Services.Accounts do
         raise "This email is already assigned to an account"
     end
   end
+
+  def authenticate_account(email, password) do
+    account = get_account_by_email(email)
+
+    case check_password(account, password) do
+      true -> {:ok, account}
+      _ -> {:error, "Invalid email or password"}
+    end
+  end
+
+  defp check_password(nil, _password), do: false
+  defp check_password(account, password) do
+    Bcrypt.verify_pass(password, account.password_hash)
+  end
 end

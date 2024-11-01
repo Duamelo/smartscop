a4bd58d1f2109d339de38b2772e16fa5e3b2f27e
dduamelo <david.dosseh@epitech.eu>
2024-11-01 16:15:44 +0100

    chore: delete useless methods

--- backend/Pscop/lib/pscop/services/roles.ex
+++ backend/Pscop/lib/pscop/services/roles.ex
@@ -5,10 +5,6 @@ defmodule Pscop.Services.Roles do
 
   alias Pscop.Schemas.Role
 
-  def list_role do
-    Repo.all(Role)
-  end
-
   def get_role!(id), do: Repo.get!(Role, id)
 
   def get_role_by_label!(label) do
@@ -22,19 +18,5 @@ defmodule Pscop.Services.Roles do
     %Role{}
     |> Role.changeset(attrs)
     |> Repo.insert()
-  end
-
-  def update_role(%Role{} = role, attrs) do
-    role
-    |> Role.changeset(attrs)
-    |> Repo.update()
-  end
-
-  def delete_role(%Role{} = role) do
-    Repo.delete(role)
-  end
-
-  def change_role(%Role{} = role, attrs \\ %{}) do
-    Role.changeset(role, attrs)
   end
 end

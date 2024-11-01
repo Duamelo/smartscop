4b8a953ac52dfe447d8d36bf7b05c0935c63b05b
VynoDePal <odjokevyn@gmail.com>
2024-11-01 14:44:16 +0100

    refactor: updating user schema

--- backend/Pscop/lib/pscop/schemas/user.ex
+++ backend/Pscop/lib/pscop/schemas/user.ex
@@ -2,14 +2,18 @@ defmodule Pscop.Schemas.User do
   use Ecto.Schema
   import Ecto.Changeset
 
+  @email_regex ~r/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+
+  @allowed_sexes ["Masculin", "Feminin", "Autre"]
+
   schema "users" do
     field :nom, :string
     field :prenoms, :string
     field :status, :string
-    field :telephone, :string
+    field :telephone, :integer
     field :email, :string
     field :sexe, :string
-    field :profession, :string
+    field :profil, :string
     field :handicap, :string
     field :pin, :string
 
@@ -20,5 +24,8 @@ defmodule Pscop.Schemas.User do
     user
     |> cast(attrs, [:nom, :prenoms, :telephone, :status, :email, :sexe, :profession, :pin, :handicap])
     |> validate_required([:nom, :prenoms])
+    |> validate_format(:email, @email_regex, message: "must be a valid email address")
+    |> validate_inclusion(:sexe, @allowed_sexes)
+    |> unique_constraint(:email)
   end
 end
--- backend/Pscop/priv/repo/migrations/0_create_user.exs
+++ backend/Pscop/priv/repo/migrations/0_create_user.exs
@@ -3,17 +3,19 @@ defmodule Pscop.Persistence.Repo.Migrations.CreateUser do
 
   def change do
     create table(:users) do
-      add :nom, :string
-      add :prenoms, :string
+      add :nom, :string, null: false
+      add :prenoms, :string, null: false
       add :telephone, :string
       add :status, :string
       add :email, :string
       add :sexe, :string
-      add :profession, :string
+      add :profil, :string
       add :pin, :string
       add :handicap, :string
 
       timestamps(type: :utc_datetime)
     end
+
+    create unique_index(:users, [:email])
   end
 end

4d1cf5b8c85bb2c72aa7587c02b899de86cb4331
dduamelo <david.dosseh@epitech.eu>
2024-11-02 02:06:01 +0100

    feat: add password validation

--- backend/Pscop/lib/pscop/schemas/account.ex
+++ backend/Pscop/lib/pscop/schemas/account.ex
@@ -4,6 +4,7 @@ defmodule Pscop.Schemas.Account do
 
   @email_regex ~r/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
   @password_regex ~r/(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_])/
+  @min_length_password 8
 
   schema "accounts" do
     field :password_hash, :string
@@ -24,23 +25,24 @@ defmodule Pscop.Schemas.Account do
     |> validate_required([:email, :password, :role_id])
     |> validate_format(:email, @email_regex, message: "must be a valid email address")
     |> unique_constraint(:email)
-    |> validate_length(:password, min: 8, message: "password must be at least 8 characters long")
-    |> validate_password_format(:password)
+    |> validate_password_format()
     |> hash_password()
   end
 
-  defp validate_password_format(changeset, field) do
-    password = get_field(changeset, field)
+  defp validate_password_format(changeset) do
+    password = get_field(changeset, :password)
 
-    if password && !valid_password?(password) do
-      add_error(changeset, field, "password must contain at least one uppercase letter, one lowercase letter, one digit, and one special character")
-    else
-      changeset
+    cond do
+      !valid_password?(password) ->
+        add_error(changeset, :password, "password must be at least 8 characters long, contain one uppercase letter, one lowercase letter, one digit, and one special character")
+        raise("password must be at least 8 characters long, contain one uppercase letter, one lowercase letter, one digit, and one special character")
+      true ->
+        changeset
     end
   end
 
   defp valid_password?(password) do
-    Regex.match?(@password_regex, password)
+    Regex.match?(@password_regex, password) && String.length(password) >= @min_length_password
   end
 
   defp hash_password(changeset) do
